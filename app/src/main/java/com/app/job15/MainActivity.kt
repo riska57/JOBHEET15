ffa27ee1ccb310e2e3b61b831292b2f02324c344
job15 <dasdwasd@gmail.com>
2022-10-18 14:33:53 +0700

    first commit

---
+++ app/src/main/java/com/app/job15/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.app.job15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnnama = findViewById<Button>(R.id.nama)
+        val btnkelas = findViewById<Button>(R.id.kelas)
+        val btnnis = findViewById<Button>(R.id.nis)
+
+        btnnama.setOnClickListener {
+            Toast.makeText(this, "Ardian Muhammad Iqbal, Toast.LENGTH_SHORT).show()
+        }
+        btnkelas.setOnClickListener {
+            Toast.makeText(this, "XI PPLG 1", Toast.LENGTH_SHORT).show()
+        }
+        btnnis.setOnClickListener {
+            Toast.makeText(this, "2976", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
